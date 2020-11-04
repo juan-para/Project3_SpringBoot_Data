8407b43a48f08f8c0d15788b95714e8fa0343768
juan-para <juanpara86@gmail.com>
2020-11-04 01:52:51 -0300

    toString Fixed

--- src/main/java/com/springbootdata/h2Project/model/Student.java
+++ src/main/java/com/springbootdata/h2Project/model/Student.java
@@ -57,6 +57,7 @@ public class Student {
 
 	@Override
 	public String toString() {
-		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", department="
+				+ department + "]";
 	}
 }
