ff259bc5a9db4917e3bcb1e2a570d29559de712a
juan-para <juanpara86@gmail.com>
2020-11-03 22:17:41 -0300

    First commit

---
+++ src/test/java/com/springbootdata/h2Project/H2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdata.h2Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
